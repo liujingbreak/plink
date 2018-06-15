815e925e6ea1abaac70ed055f24966fe9c51aab6
Liu Jing <jing.liu@bkjk.com>
2018-06-15 12:00:53 +0800

    Fix file loader

--- src/internal/webpack2-builder/lib/dr-file-loader.js
+++ src/internal/webpack2-builder/lib/dr-file-loader.js
@@ -6,6 +6,7 @@ var path = require('path').posix;
 var loaderUtils = require('loader-utils');
 var api = require('__api');
 var _ = require('lodash');
+const fs = require('fs');
 // var log = require('log4js').getLogger('wfh.dr-file-loader');
 var resolveSymlink = null;
 
@@ -20,18 +21,12 @@ module.exports = function(content) {
 		if (!this.emitFile) throw new Error('emitFile is required from module system');
 
 		var query = loaderUtils.getOptions(this) || {};
-		// var configKey = query.config || 'fileLoader';
-		// var options = this.options[configKey] || {};
+
 		var config = {
 			publicPath: false,
 			useRelativePath: false,
 			name: '[path][name].[md5:hash:hex:8].[ext]' // hack
 		};
-
-		// options takes precedence over config
-		// Object.keys(options).forEach(function(attr) {
-		// 	config[attr] = options[attr];
-		// });
 
 		// query takes precedence over config and options
 		Object.keys(query).forEach(function(attr) {
@@ -47,18 +42,17 @@ module.exports = function(content) {
 
 		var filePath = this.resourcePath;
 		var browserPackage = api.findPackageByFile(filePath);
-		// if (browserPackage) {
 		let outputPath = _.trimStart(api.config.get(['outputPathMap', browserPackage.longName]), '/');
 
 		let packageDir;
 		if (browserPackage.realPackagePath.startsWith(process.cwd()) || resolveSymlink) {
 			packageDir = browserPackage.realPackagePath;
+			filePath = fs.realpathSync(filePath);
 		} else {
 			packageDir = browserPackage.packagePath;
 		}
 		outputPath = path.join(outputPath, path.dirname(path.relative(packageDir, filePath)));
 		url = path.join(outputPath, url.split('/').pop()); // only file name part
-		// } else
 		url = url.replace(/(^|\/)node_modules(\/|$)/g, '$1n-m$2').replace(/@/g, 'a');
 
 		var publicPath = '__webpack_public_path__ + ' + JSON.stringify(url);
