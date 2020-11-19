172d0eb4696dedd21f9605b162bb426381a24576
Liu Jing <jing.liu@bkjk.com>
2020-11-19 20:08:51 +0800

    Thread pool - WIP

--- main/wfh/ts/thread-pool/index.ts
+++ main/wfh/ts/thread-pool/index.ts
@@ -1,12 +1,24 @@
-// import {Worker} from 'worker_threads';
-import {} from './promise-queque';
+import {Worker} from 'worker_threads';
+import {queue} from './promise-queque';
+// import Path from 'path';
 
 export default function newThreadPool(maxParalle: number, idleTimeMs: number) {
 
 }
 
 export class Pool {
-  constructor(maxParalle: number, idleTimeMs: number) {
+  private promiseQ: ReturnType<typeof queue>;
+  private workers: Worker[] = [];
 
+  constructor(maxParalle: number, idleTimeMs: number) {
+    this.promiseQ = queue(maxParalle);
+  }
+
+  submit<T>(task: () => Promise<T>): Promise<T> {
+    return this.promiseQ.add(task);
+  }
+
+  protected createWorker() {
+    this.workers.push(new Worker(require.resolve('./worker')));
   }
 }
