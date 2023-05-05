a017ead107a684b741640dc6891ae7ccc96f10d0
Liu Jing <liujing.break@163.com>
2023-05-05 15:49:48 +0800

    fix(doc-entry): ESlint message

---
+++ doc-app/.eslintrc.js
@@ -0,0 +1,9 @@
+const config = require('@wfh/eslint-config-cra').create(
+  require('path').resolve(__dirname, 'tsconfig.json'),
+  '17.0.2' // React version
+);
+
+// To change default ignorePatterns
+// config.ignorePatterns = ["**/*.d.ts"];
+
+module.exports = config;
